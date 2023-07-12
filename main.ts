1703a71fa4b714db3837a30057956725bca66b2f
ZK <124562280+P55D2K@users.noreply.github.com>
2023-07-12 11:29:50 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,25 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (`A${ascore}`))
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("" + (`A${ascore} B${bscore} ${ascore > bscore ? "A Wins" : bscore > ascore ? "B Wins" : "Tie"}`))
+    basic.showString("Resetting Scores")
+    ascore = 0
+    bscore = 0
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("" + (`B${bscore}`))
+})
+input.onGesture(Gesture.Shake, function () {
+    index = randint(1, 3)
+    if (index == 0) {
+        basic.showIcon(IconNames.Scissors)
+    } else if (index == 1) {
+        basic.showIcon(IconNames.Square)
+    } else if (index == 2) {
+        basic.showIcon(IconNames.SmallSquare)
+    }
+})
+let index = 0
+let bscore = 0
+let ascore = 0
